fb714a37490d9eacb7ee06ca5faf977f5a740005
Serena-Dong <serenadong3159@icloud.com>
2023-01-24 16:19:35 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default {
+    components: { AppHeader, AppMain, AppFooter }
+}
 </script>
 
 <template>
-
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+    <AppFooter></AppFooter>
 </template>
 
 <style scoped>
---
+++ src/components/contents/AppContents.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'AppContents',
+    props: {
+        content: Object
+    }
+}
+</script>
+
+<template>
+
+    <div class="card">
+        <a href="">
+            <img :src="content.thumb" :alt="content.type">
+
+            <p>{{ content.series }}</p>
+        </a>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+
+a {
+    color: white;
+
+    &:hover {
+        color: $dc_blue;
+    }
+}
+
+.card {
+    width: 180px;
+    height: 270px;
+
+    img {
+        object-fit: cover;
+        width: 180px;
+        height: 180px;
+
+        &:hover {
+            border: $dc_blue 2px solid;
+        }
+    }
+
+    p {
+        margin: 1rem 0 1rem
+    }
+}
+</style>
